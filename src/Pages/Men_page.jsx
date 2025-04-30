1cdf3cd4d64e5c58e693ded02bfb14fe1246e176
AlfiyaHilal <alfiyas2112@gmail.com>
2025-04-30 10:54:04 +0530

    Add files via upload

---
+++ src/Pages/Men_page.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import styled from 'styled-components'
+import Navbar from '../components/Navbar'
+import Footer from '../components/Footer'
+import Men from '../components/Men'
+import  '../App.css'
+
+
+export default function Men_page() {
+  return (
+    <div>
+        
+        <Navbar/>
+        <Men />
+        <Footer/>
+    </div>
+  )
+}
+
